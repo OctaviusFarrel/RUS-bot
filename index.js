3ee0f4c9e04d77185b1a74fe27ebcf5f843c55a4
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-08-17 04:32:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function stop(message) {
 	if (!userVoiceChannel) return;
 	let clientVoiceConnection = message.guild.voice;
 	if (clientVoiceConnection == undefined) message.reply("aku tidak di dalam voice channel apapun :)");
-	else if (clientVoiceConnection.channel.type == 'voice') message.reply('masuk voice chat nya dulu napa ?');
+	else if {(clientVoiceConnection.channel.type == 'voice') message.reply('masuk voice chat nya dulu napa ?');}
 	else if (userVoiceChannel.channel === clientVoiceConnection.channel) {
 		if (message.member.hasPermission('ADMINISTRATOR')) {
 			clientVoiceConnection.channel.leave();
