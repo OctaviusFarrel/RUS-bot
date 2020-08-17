2ee11581e89433c2767830b12cc500a297a14906
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-08-17 14:32:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -46,49 +46,51 @@ async function stop(message) {
 }
 
 client.on('ready', () => {
-	client.user.setStatus('online', "rus!help").catch(err => console.log(err));
 	client.user.setPresence({
-        game: {
-            name: 'rus!help',
-        }
-    });
-  client.user.setAvatar(ProfilePicture).catch(err => console.log(err));
-  console.log('I am ready!');
+		status:'online',
+        	activity: {
+			name:'rus!help',
+			type:'STREAMING',
+			url:'https://smkrus.sch.id/'
+        	}
+    	});
+	client.user.setAvatar(ProfilePicture).catch(err => console.log(err));
+	console.log('I am ready!');
 });
 
 client.on('message', message => {
 	var MsgLow = message.content.toLowerCase();
 	if (MsgLow.startsWith(prefix)) {
 		MsgLow = MsgLow.slice(4)
-		console.log(MsgLow)
-		switch (MsgLow) {
-			case "invite":
-				const EmbedInvite = new Discord.MessageEmbed()
+		if (MsgLow == 'invite') {
+			const EmbedInvite = new Discord.MessageEmbed()
 				.setColor('#0099ff')
 				.setTitle('Klik link')
 				.setURL('https://discord.com/api/oauth2/authorize?client_id=744547971353411585&scope=bot&permissions=8')
 				.setTimestamp();
-				message.channel.send(EmbedInvite);
-				break;
-			case "help":
-				MsgLow = "";
-				for (const [key,value] of Object.entries(CommandList)) {
-					MsgLow = MsgLow.concat("`"+key+"` - "+value+"\n")
-				}
-				const EmbedHelp = new Discord.MessageEmbed()
+			message.channel.send(EmbedInvite);
+		} else if (MsgLow == 'help') {
+			MsgLow = "";
+			for (const [key,value] of Object.entries(CommandList)) {
+				MsgLow = MsgLow.concat("`"+key+"` - "+value+"\n")
+			}
+			const EmbedHelp = new Discord.MessageEmbed()
 				.setColor('#0099ff')
 				.setTitle('List Perintah')
 				.setDescription(MsgLow)
 				.setTimestamp();
-				message.channel.send(EmbedHelp);
-				break;
-			case "pekora":
-				if (message.member.voice.channel) {
-					play('https://www.youtube.com/watch?v=7dNrO7TSZdU',message)
-				} else {
-					message.reply('Join voice chat dlu goblok!');
-				}
-				break;
+			message.channel.send(EmbedHelp);
+		} else if (MsgLow == "pekora") {
+			if (message.member.voice.channel) {
+				play('https://www.youtube.com/watch?v=7dNrO7TSZdU',message)
+			} else {
+				message.reply('Join voice chat dlu goblok!');
+			}
+		} else if (MsgLow == 'dc'||MsgLow == 'disconnect'||MsgLow == 'leave') {
+			stop(message);
+		} else if (MsgLow == "info") {
+			message.channel.send("`Version : 1.0.1\nAuthor : OkutaJager\nCondition : Unstable\nMay crash anytime`");
+		} else message.channel.send("pake rus!help dlu sana")
 			/*case "play":
 			case "p":
 				var
@@ -98,15 +100,6 @@ client.on('message', message => {
 					message.reply('Join voice chat dlu goblok!');
 				}
 				break;*/
-			case "info":
-				message.channel.send("`Version : 1.0.1\nAuthor : OkutaJager\nCondition : Unstable\nMay crash anytime`")
-				break;
-			case "dc":
-			case "disconnect":
-			case "leave":
-				console.log("masuk")
-				stop(message);
-				break;
 			/*case "report":
 				message.channel.send("`Command report ini akan langsung mengarah ke DM author (OkutaJager), silahkan ketik bug, dsb setelah membaca pesan ini`")
 				const reportCollector = new Discord.MessageCollector(message.channel,response => response.author.id == message.author.id ,{time:100000});
@@ -114,21 +107,7 @@ client.on('message', message => {
 					report(message,myid,response);
 				});
 				break;*/
-			default:
-				message.channel.send("pake rus!help dlu sana")
-		}
 	}
 })
-
-/*client.on('message', async message => {
-	if (message.member.voice.channel) {
-		if (message.content.toLowerCase() == "rus!pekora") {
-			const connection = await message.member.voice.channel.join();
-			connection.play(ytdl('https://www.youtube.com/watch?v=7dNrO7TSZdU', { filter: 'audioonly' }));
-		}
-	} else {
-		message.reply('Join voice chat dlu goblok!');
-	}
-});*/
 
 client.login(process.env.token);
