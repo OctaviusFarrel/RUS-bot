08ac21257ad3686da95b68ba1cd1776b522c07dd
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2021-09-16 18:32:56 +0700

    feat: Adding voice channel feature 👍

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 require('dotenv').config();
+const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
 const { Client, Intents, MessageEmbed } = require("discord.js");
 
+function embedMaker(title, description, url = "",) {
+	return new MessageEmbed()
+	.setTitle(title)
+	.setDescription(description)
+	.setColor('#0099ff')
+	.setURL(url)
+	.setThumbnail(client.user.avatarURL())
+	.setFooter('Bot RUS');
+}
 
 const client = new Client(
 	{
 		intents:[
 			Intents.FLAGS.GUILDS, 
 			Intents.FLAGS.GUILD_MESSAGES,
-			Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
+			Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+			Intents.FLAGS.GUILD_VOICE_STATES]
 		});
+
+const botVoiceChannels = new Map();
+
 
 client.once('ready', () => {
 	console.log("I'm Ready");
@@ -24,10 +38,7 @@ client.once('ready', () => {
 });
 
 client.on('guildCreate', guild => {
-	let embed = new MessageEmbed()
-	.setTitle("Terimakasih sudah menambahkanku!")
-	.setColor('#0099ff')
-	.setFooter('Bot RUS');
+	let embed = embedMaker("Terimakasih sudah menambahkanku!");
 	guild.systemChannel.send({embeds: [embed]});
 });
 
@@ -38,15 +49,28 @@ client.on('interactionCreate', async interaction => {
 				await interaction.reply("Pong!");
 				break;
 			case "invite":
-				let embed = new MessageEmbed()
-				.setTitle("Tambahkan Bot (Click di sini)")
-				.setURL("https://discord.com/api/oauth2/authorize?client_id=744547971353411585&permissions=8&scope=bot%20applications.commands")
-				.setDescription("Untuk dipahami bahwa bot hanya bisa ditambahkan oleh pemilik bot, kontak OkutaJager#7771 untuk info lebih lanjut")
-				.setFooter("Bot RUS");
+				let embed = embedMaker(
+					"Tambahkan Bot (Click di sini)", 
+					"Perlu dipahami bahwa bot hanya bisa ditambahkan oleh pemilik bot, kontak OkutaJager#7771 untuk info lebih lanjut",
+					"https://discord.com/api/oauth2/authorize?client_id=744547971353411585&permissions=8&scope=bot%20applications.commands")
 				await interaction.reply({embeds:[embed],ephemeral:true})
+			case "keluar":
+				break;
 			case "temenin":
-				if (interaction.member.voice) {
-					console.log("inside!");
+				if (botVoiceChannels.get(interaction.guildId)) {
+					await interaction.reply({content: "Saya sedang menemani seseorang di voice channel lain!", ephemeral: true});
+					return;
+				}
+				const voiceChannel = interaction.member.voice.channel;
+				if (voiceChannel) {
+					const connection = joinVoiceChannel({
+						channelId: voiceChannel.id,
+						guildId: voiceChannel.guild.id,
+						adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+						selfDeaf: false
+					})
+					botVoiceChannels.set(interaction.guildId, connection);
+					await interaction.reply("Saya sudah join!");
 				} else await interaction.reply({content: "Anda tidak di dalam voice channel!", ephemeral: true});
 				break;
 			default:
